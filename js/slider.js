e7250ccd917d471e74da5a7c5328e82be911571a
roff-dev <kieronoates@hotmail.co.uk>
2024-10-29 11:49:35 +0000

    side menu creation and styling
    js functionality needed

--- js/slider.js
+++ js/slider.js
@@ -4,32 +4,3 @@ $('.slick-slider').slick({
     cssEase: 'linear'
     
 });
-// $('.slick-dots').css({
-//     'position': 'absolute',
-//     'bottom': '20px',
-//     'left': '50%',
-//     'transform': 'translateX(-50%)',
-//     'display': 'flex',
-//     'justify-content': 'center',
-//     'list-style': 'none',
-//     'padding': '0',
-//     'margin': '0'
-//   });
-  
-//   $('.slick-dots li').css({
-//     'margin': '0 5px'
-//   });
-  
-//   $('.slick-dots li button').css({
-//     'width': '12px',
-//     'height': '12px',
-//     'background-color': '#ccc',
-//     'border-radius': '50%',
-//     'border': '2px solid transparent',
-//     'transition': 'all 0.3s ease'
-//   });
-  
-//   $('.slick-dots li.slick-active button').css({
-//     'background-color': '#fff',
-//     'border': '2px solid #333'
-//   });
