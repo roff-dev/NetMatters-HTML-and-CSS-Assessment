1ec555c24b22afa28c88df7b914a3a5084d300ae
roff-dev <kieronoates@hotmail.co.uk>
2024-11-04 11:48:25 +0000

    tooltips working with slick

--- js/slider.js
+++ js/slider.js
@@ -4,3 +4,23 @@ $('.slick-slider').slick({
     cssEase: 'linear'
     
 });
+$(document).ready(function(){
+    // Destroy any existing slick initialization
+    if ($('.logo-wrapper').hasClass('slick-initialized')) {
+        $('.logo-wrapper').slick('unslick');
+    }
+    
+    // Reinitialize with new settings
+    $('.logo-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        cssEase: 'linear',
+        centerMode: false,
+        variableWidth: true,
+    });
+});
