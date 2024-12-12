8a5350ea1c01ba2aa435dcd70c2da37a9cd7d928
roff-dev <kieronoates@hotmail.co.uk>
2024-12-12 16:11:29 +0000

    contact structure

--- contact.php
+++ contact.php
@@ -3,6 +3,52 @@ include ("inc/header.php");
 ?>
 
 
+<div class="content-wrapper">
+    <div class="location-bar">
+        <div class="container">
+            <ul class="breadcrumb">
+                <li><a href="#">Home</a></li>
+                <li>/ Our Offices</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="contact-header">
+        <div class="container">
+            <h1>Our Offices</h1>
+        </div>
+    </div>
+
+    <div class="offices">
+        <div class="container">
+            <div class="office-cards">
+                <div class="office-1">
+                    <img src="" alt="">
+                    <div class="office-content">
+
+                    </div>
+                </div>
+                <div class="office-2">
+                    <img src="" alt="">
+                    <div class="office-content">
+                        
+                    </div>
+                </div>
+                <div class="office-3">
+                <img src="" alt="">
+                    <div class="office-content">
+                        
+                    </div>
+                </div>
+            </div>  
+        </div>
+    </div>
+
+
+</div>
+
+
+
 <?php
 include ("inc/footer.php");
 ?>
