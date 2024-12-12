fabd03c00aacfa8c7d54d04e201b6b1f4fcfda26
roff-dev <kieronoates@hotmail.co.uk>
2024-12-12 09:43:57 +0000

    footer include

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include ("inc/header.php")
+include ("inc/header.php");
 
 ?>
     
@@ -415,90 +415,8 @@ include ("inc/header.php")
     </div>
     
 
-    <footer>
-        <div class="container">
-            <div class="footer-social">
-                <div class="net-img">
-                    <img src="img/nm-logo-white.png" alt="Netmatters Logo">
-                </div>
-                <h4>FOLLOW US ON:</h4>
-                <div class="social-btn">
-                    <a class="social-btn facebook" href="#"><span class="icon-facebook"></span></a>
-                    <a class="social-btn linkedin" href="#"><span class="icon-linkedin"></span></a>
-                    <a class="social-btn twitter" href="#"><span class="icon-twitter"></span></a>
-                    <a class="social-btn instagram" href="#"><span class="icon-instagram"></span></a>
-                </div>
-            </div>
-            
-            <div class="footer-row">
-                
-                <div class="footer-menu">
-                    <h4>About Netmatters</h4>
-                    <ul>
-                        <li><a href="#">Our Careers</a></li>
-                        <li><a href="#">Our Team</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Cookie Policy</a></li>
-                        <li><a href="#">Data Retention & Disposal Policy</a></li>
-                        <li><a href="#">CCTV Policy</a></li>
-                        <li><a href="#">Environmental Policy</a></li>
-                        <li><a href="#">Terms & Conditions</a></li>
-                    </ul>
-                </div>
+<?php
 
-                <div class="footer-menu">
-                    <h4>Our Services</h4>
-                    <ul>
-                        <li><a href="#">Bespoke Software</a></li>
-                        <li><a href="#">IT Support</a></li>
-                        <li><a href="#">Digital Marketing</a></li>
-                        <li><a href="#">Telecoms Services</a></li>
-                        <li><a href="#">Web Design</a></li>
-                        <li><a href="#">Cyber Security</a></li>
-                        <li><a href="#">Developer Training</a></li>
-                    </ul>
-                </div>
+include ("inc/footer.php");
 
-                <div class="footer-menu">
-                    <h4>Our Industries</h4>
-                    <ul>
-                        <li><a href="#">Financial Services</a></li>
-                        <li><a href="#">Construction</a></li>
-                        <li><a href="#">Retail & E-Commerce</a></li>
-                        <li><a href="#">Non-Profits</a></li>
-                        <li><a href="#">SME's</a></li>
-                        <li><a href="#">Healthcare</a></li>
-                        <li><a href="#">Education & Training</a></li>
-                        <li><a href="#">Travel & Leisure</a></li>
-                    </ul>
-                </div>
-
-                <div class="footer-menu">
-                    <h4>Locations</h4>
-                    <ul>
-                        <li><a href="#">Cambridge Office</a></li>
-                        <li><a href="#">Wymondham Office</a></li>
-                        <li><a href="#">Great Yarmouth Office</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        
-    </footer>
-
-            <div class="footer-last">
-                <div class="container">
-                    <div class="license">
-                        &copy; Copyright Netmatters 2024. All rights reserved. - <a href="#">Sitemap</a>
-                    </div>
-                </div>
-            </div>
-        <script src="js/jquery-3.7.1.min.js"></script>   
-        <script src="js/slick/slick.min.js"></script>
-        <script src="js/slider.js"></script>
-        <script src="js/side.js"></script>
-        <script src="js/main.js"></script>
-    
-    </body>
-</html>
+?>
